88bf2bf9f23727d470d5fdb87cba1eeb75685a0e
DanielDiasGuimaraes <112535232+DanielDiasGuimaraes@users.noreply.github.com>
2025-06-08 09:52:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function enviarFormulario() {
+  const dados = {
+    nome: document.getElementById("nome").value,
+    data_nascimento: document.getElementById("data_nascimento").value,
+    telefone: document.getElementById("telefone").value,
+    email: document.getElementById("email").value,
+    cpf: document.getElementById("cpf").value,
+    curriculo: document.getElementById("curriculo").value
+  };
+
+  fetch("https://vagas-q7sc.onrender.com/enviar", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dados)
+  })
+  .then(res => res.json())
+  .then(res => {
+    document.getElementById("mensagem").innerText = "Enviado com sucesso!";
+    document.getElementById("mensagem").style.color = "green";
+  })
+  .catch(err => {
+    document.getElementById("mensagem").innerText = "Erro ao enviar!";
+    document.getElementById("mensagem").style.color = "red";
+  });
+}
